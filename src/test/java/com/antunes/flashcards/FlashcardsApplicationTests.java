e0052c3c954d6791a9fb83719c8d6ed89fbfb879
Celso Antunes <celsoccaj@hotmail.com>
2025-04-09 14:05:18 +0200

    Test commit with formatter hook

--- src/test/java/com/antunes/flashcards/FlashcardsApplicationTests.java
+++ src/test/java/com/antunes/flashcards/FlashcardsApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class FlashcardsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }

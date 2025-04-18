bf2ea674f70ea042fe018465169991c71f59656b
Celso Antunes <celsoccaj@hotmail.com>
2025-04-18 11:52:10 +0200

    feat(auth): Implement login with correct password

---
+++ src/main/java/com/antunes/flashcards/domain/user/auth/LoginService.java
@@ -0,0 +1,38 @@
+package com.antunes.flashcards.domain.user.auth;
+
+import com.antunes.flashcards.domain.user.exception.PasswordValidationException;
+import com.antunes.flashcards.domain.user.exception.UserNotFoundException;
+import com.antunes.flashcards.domain.user.model.Email;
+import com.antunes.flashcards.domain.user.model.User;
+import com.antunes.flashcards.domain.user.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public LoginService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  public String login(String rawEmail, String rawPassword) {
+    Email email = new Email(rawEmail);
+    Optional<User> userOptional = userRepository.findByEmail(email);
+    if (userOptional.isEmpty()) {
+      throw new UserNotFoundException("User not found");
+    }
+    User user = userOptional.get();
+    String storedHashed = user.getHashedPassword();
+    if (!passwordEncoder.matches(rawPassword, storedHashed)) {
+      throw new PasswordValidationException("Incorrect password");
+    }
+
+    return "dummy-token";
+  }
+}
--- src/test/java/com/antunes/flashcards/domain/user/auth/AuthenticationUnitTests.java
+++ src/test/java/com/antunes/flashcards/domain/user/auth/AuthenticationUnitTests.java
@@ -3,7 +3,6 @@ package com.antunes.flashcards.domain.user.auth;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import com.antunes.flashcards.domain.user.PasswordFactory;
 import com.antunes.flashcards.domain.user.model.Email;
 import com.antunes.flashcards.domain.user.model.Password;
 import com.antunes.flashcards.domain.user.model.StubPasswordEncoder;
@@ -25,22 +24,20 @@ public class AuthenticationUnitTests {
   private LoginService loginService;
 
   @Mock UserRepository userRepository;
-  @Mock PasswordFactory passwordFactory;
   private final PasswordEncoder passwordEncoder = new StubPasswordEncoder();
 
   @BeforeEach
   void setUp() {
-    loginService = new LoginService(userRepository, passwordFactory, passwordEncoder);
+    loginService = new LoginService(userRepository, passwordEncoder);
   }
 
   @Test
   void registeredUserCanLoginCorrectPassword() {
     Email email = new Email(rawEmail);
     Password mockedPassword = mock(Password.class);
-    when(passwordFactory.create(rawPassword)).thenReturn(mockedPassword);
+    when(mockedPassword.getHashedPassword()).thenReturn("$2stub$" + rawPassword);
     User mockedUser = new User(email, mockedPassword);
     when(userRepository.findByEmail(email)).thenReturn(Optional.of(mockedUser));
-    when(passwordEncoder.matches(rawPassword, "$2stub$" + rawPassword)).thenReturn(true);
     String token = loginService.login(rawEmail, rawPassword);
     assertNotNull(token);
   }
